4f976c11c70c4f27edf6147de6ad474e4a095b13
jingling jin <jljin0524@gmail.com>
2020-06-19 04:00:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function add(a,b){
+  return a+b
+}
+function subtract(a,b) {
+  return a-b
+}
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+var n = 1
+function increment(n){
+  n++
+  return n
+}
+function decrement(n){
+  n --
+  return n
+}
+function makeInt(n){
+  parseInt('n', 10)
+  return n
+}
